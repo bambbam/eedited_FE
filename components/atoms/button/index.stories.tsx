56d5a2398b4d19d95eb115fe27ab0bb0afe3ef44
JeonMinSu <minsu2530@u.sogang.ac.kr>
2021-11-22 15:07:03 +0900

    [ADD]storybook

--- components/atoms/button/index.stories.tsx
+++ components/atoms/button/index.stories.tsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-
-
-import  Button  from '.';
-import * as styles from "./style.module.scss"
+import Button, { Props } from '.';
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Button',
-  component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+    title: 'Example/Button',
+    component: Button,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
 } as ComponentMeta<typeof Button>;
 
-const TemplateBtn: ComponentStory<typeof Button> = (args) => <Button {...args}>template</Button>;
+const TemplateBtn: ComponentStory<typeof Button> = (args: Props) => (
+    <Button {...args}>로그인</Button>
+);
 
 export const Primary = TemplateBtn.bind({});
 Primary.args = {
-}
+    theme: 'gray',
+};
 export const Large = TemplateBtn.bind({});
-
 Large.args = {
-  size: "large",
-  theme: "orange",
+    size: 'large',
+    theme: 'gray',
+};
+export const Small = TemplateBtn.bind({});
+Small.args = {
+    size: 'small',
+    theme: 'gray',
 };
